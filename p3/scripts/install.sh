261facefbfb91c4c0f593137465eb798dd200c7a
LXV <lxvsws@gmail.com>
2025-10-10 13:47:30 +0200

    save

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# Install k3d
+# Install dependencies
+sudo apt update && sudo apt install apt-transport-https ca-certificates curl gnupg lsb-release
+
+# Install Docker
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update && sudo apt install docker-ce docker-ce-cli containerd.io
+
+# Install K3D
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
 # Install kubectl
@@ -20,7 +28,7 @@ kubectl create namespace dev
 # Install Argo CD
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-# Apply Argo CD Application manifest
+# Apply Argo CD application manifest
 kubectl apply -f ../confs/argo.yaml
 
 echo "✅ Initial setup complete"
