b660843626443dee21c1185176395519cdf126b7
LXV <lxvsws@gmail.com>
2025-10-10 13:54:00 +0200

    save

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Install dependencies
-sudo apt update && sudo apt install apt-transport-https ca-certificates curl gnupg lsb-release
+sudo apt update && sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
 
 # Install Docker
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update && sudo apt install docker-ce docker-ce-cli containerd.io
+sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER && newgrp docker
 
 # Install K3D
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
